5dacc3d1e0e28d3e64f4f090ae4da85f6d4d34d5
irpsv <i.rpsv@live.com>
2016-12-15 21:45:49 +0500

    update prepareValue

--- src/field/NumberField.php
+++ src/field/NumberField.php
@@ -4,25 +4,13 @@ namespace jugger\ar\field;
 
 class NumberField extends BaseField
 {
-    /**
-     * Масштаб
-     * Число знаков в целой и дробной частях
-     * @var integer
-     */
-    public $scale;
-    /**
-     * Число знаков после запятой
-     * @var integer
-     */
-    public $accuracy;
-
-    protected function prepareValue()
+    protected function prepareValue($value)
     {
-        if (is_numeric($this->value)) {
-            return (float) $this->value;
+        if (is_numeric($value)) {
+            return (float) $value;
         }
         else {
-            throw new ValueException($this);
+            return null;
         }
     }
 }
