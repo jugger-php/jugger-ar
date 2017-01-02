9dfbb0bc4b1555408e773ee6cae0b3cfffff60e2
irpsv <i.rpsv@live.com>
2017-01-02 19:06:36 +0500

    add DI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,18 +5,26 @@ use jugger\db\ConnectionPool;
 // composer vendor autoload
 include __DIR__ .'/../../../autoload.php';
 
-ConnectionPool::getInstance()->init([
+class Di
+{
+    public static $pool;
+}
+
+Di::$pool = new ConnectionPool([
     'default' => [
-        'class' => 'jugger\db\driver\PdoConnection',
-        'dsn' => 'sqlite::memory:',
-    ]
+        'class' => 'jugger\db\driver\MysqliConnection',
+        'host' => 'localhost',
+        'dbname' => 'test',
+        'username' => 'root',
+        'password' => '',
+    ],
 ]);
 
 spl_autoload_register(function($class) {
     if (substr($class, 0, 5) != "tests") {
         return;
     }
-    
+
     $class = substr($class, 6);
     $file = __DIR__ ."/lib/{$class}.php";
     if (file_exists($file)) {
