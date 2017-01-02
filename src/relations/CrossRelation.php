fda311b2d68662819a78e17375b58c5d0d27b107
irpsv <i.rpsv@live.com>
2017-01-02 19:04:57 +0500

    update CrossRelation

--- src/relations/CrossRelation.php
+++ src/relations/CrossRelation.php
@@ -2,36 +2,81 @@
 
 namespace jugger\ar\relations;
 
-use jugger\ar\mapping\AssociationKey;
+use jugger\ar\ActiveRecord;
 
-class CrossRelation extends AssociationKey implements RelationInterface
+class CrossRelation
 {
-    protected $many;
-    protected $value;
-    protected $className;
+    protected $vias = [];
+    protected $target;
+    protected $selfColumn;
 
-    public function __construct(array $keys, string $className, bool $many)
+    public function __construct(string $selfColumn)
     {
-        $this->many = $many;
-        $this->className = $className;
-        // set table name in last index
-        $keys[count($keys) - 1][2] = $className::getTableName();
-
-        parent::__construct($keys);
+        $this->selfColumn = $selfColumn;
     }
 
-    public function getValue($selfValue)
+    public function getVias(ActiveRecord $model)
     {
-        if (!$this->value) {
-            $class = $this->className();
-            $query = $class::find();
+        $ret = [];
+        $c1 = $this->selfColumn;
+        $t1 = $model::getTableName();
 
-            foreach ($this->keys as $key) {
-                $query->joinForeignKey($key);
-            }
+        foreach ($this->vias as $via) {
+            $c2 = $via['prevColumn'];
+            $t2 = $via['table'];
 
-            $this->value = $this->many ? $query->all() : $query->one();
+            array_unshift($ret, [$t1, $c1, $t2, $c2]);
+            $t1 = $t2;
+            $c1 = $via['nextColumn'];
         }
-        return $this->value;
+
+        $c2 = $this->target['column'];
+        $t2 = $this->target['class']::getTableName();
+        array_unshift($ret, [$t1, $c1, $t2, $c2]);
+        return $ret;
+    }
+
+    public function getQuery(ActiveRecord $model)
+    {
+        $class = $this->target['class'];
+        $db = $class::getDb();
+        $vias = $this->getVias($model);
+        $query = $class::find();
+
+        foreach ($vias as $via) {
+            list($t2, $c2, $t1, $c1) = $via;
+            $query->innerJoin(
+                $db->quote($t2),
+                $db->quote("{$t1}.{$c1}") ." = ". $db->quote("{$t2}.{$c2}")
+            );
+        }
+        $query->where([
+            "{$t2}.{$c2}" => $model[$c2]
+        ]);
+
+        return $query;
+    }
+
+    public function getValue(ActiveRecord $model)
+    {
+        return $this->getQuery($model)->all();
+    }
+
+    /**
+     * Промежуточная таблица
+     */
+    public function via(string $prevColumn, string $nextColumn, string $table)
+    {
+        $this->vias[] = compact('prevColumn', 'nextColumn', 'table');
+        return $this;
+    }
+
+    /**
+     * Конечная таблица
+     */
+    public function target(string $column, string $class)
+    {
+        $this->target = compact('column', 'class');
+        return $this;
     }
 }
