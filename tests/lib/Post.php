43b80ab1b8b93fba48eaa6f1127e0cd907983a14
irpsv <i.rpsv@live.com>
2017-01-07 16:23:02 +0500

    refact & extends Model

--- tests/lib/Post.php
+++ tests/lib/Post.php
@@ -2,43 +2,49 @@
 
 namespace tests;
 
+use jugger\db\ConnectionInterface;
 use jugger\ar\ActiveRecord;
-use jugger\ar\field\TextField;
-use jugger\ar\field\IntegerField;
 use jugger\ar\relations\ManyRelation;
+use jugger\ar\validator\PrimaryValidator;
+use jugger\model\Model;
+use jugger\model\field\TextField;
+use jugger\model\field\IntField;
+use jugger\model\validator\RangeValidator;
 
 class Post extends ActiveRecord
 {
-    public static function getTableName()
+    public static function getTableName(): string
     {
         return 'post';
     }
 
-    public static function getDb()
+    public static function getDb(): ConnectionInterface
     {
         return static::$_db ?? \Di::$pool['default'];
     }
 
-    public static function getSchema()
+    public static function getSchema(): array
     {
         return [
-            new IntegerField([
-                'column' => 'id',
-                'primary' => true,
-                'autoIncrement' => true,
+            new IntField([
+                'name' => 'id',
+                'validators' => [
+                    new PrimaryValidator(),
+                ],
             ]),
             new TextField([
-                'column' => 'title',
-                'length' => 100,
+                'name' => 'title',
+                'validators' => [
+                    new RangeValidator(1, 100)
+                ],
             ]),
             new TextField([
-                'column' => 'content',
-                'default' => null,
+                'name' => 'content',
             ]),
         ];
     }
 
-    public static function getRelations()
+    public static function getRelations(): array
     {
         return [
             'authors' => new ManyRelation('id', 'id_post', 'tests\Author'),
