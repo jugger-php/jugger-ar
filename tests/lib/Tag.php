00034bf13242b6c0fec936a999bf1bb15ad1912f
irpsv <i.rpsv@live.com>
2017-01-07 16:23:06 +0500

    refact & extends Model

--- tests/lib/Tag.php
+++ tests/lib/Tag.php
@@ -2,39 +2,45 @@
 
 namespace tests;
 
+use jugger\db\ConnectionInterface;
 use jugger\ar\ActiveRecord;
-use jugger\ar\field\TextField;
-use jugger\ar\field\IntegerField;
 use jugger\ar\relations\ManyRelation;
+use jugger\model\Model;
+use jugger\model\field\TextField;
+use jugger\model\field\IntField;
+use jugger\model\validator\RangeValidator;
 
 class Tag extends ActiveRecord
 {
-    public static function getTableName()
+    public static function getTableName(): string
     {
         return 'tag';
     }
 
-    public static function getDb()
+    public static function getDb(): ConnectionInterface
     {
         return static::$_db ?? \Di::$pool['default'];
     }
 
-    public static function getSchema()
+    public static function getSchema(): array
     {
         return [
-            new IntegerField([
-                'column' => 'id',
-                'primary' => true,
-                'autoIncrement' => true,
+            new IntField([
+                'name' => 'id',
+                'validators' => [
+                    new PrimaryValidator()
+                ],
             ]),
             new TextField([
-                'column' => 'value',
-                'length' => 100,
+                'name' => 'value',
+                'validators' => [
+                    new RangeValidator(1, 100)
+                ],
             ]),
         ];
     }
 
-    public static function getRelations()
+    public static function getRelations(): array
     {
         return [
             'authors' => (new CrossRelation('id'))->via('id_tag', 'id_post', 'post_tag')->target('id', 'tests\Post'),
