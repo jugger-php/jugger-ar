17a5571752bf734a016eb92799fadddc5174e235
irpsv <i.rpsv@live.com>
2017-02-25 21:17:47 +0500

    refact

--- src/validator/PrimaryValidator.php
+++ src/validator/PrimaryValidator.php
@@ -2,8 +2,16 @@
 
 namespace jugger\ar\validator;
 
-use jugger\model\validator\RequireValidator;
+use jugger\model\validator\BaseValidator;
 
-class PrimaryValidator extends RequireValidator
+class PrimaryValidator extends BaseValidator
 {
+    /**
+     * Возвращает всегда TRUE, чтобы можно было использовать поле как AUTO_INCREMENT:
+     * При значении NULL - значение автоматически подставляется самой базой
+     */
+    public function validate($value): bool
+    {
+        return true;
+    }
 }
