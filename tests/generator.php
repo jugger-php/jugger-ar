de47cacab72bde1074dc0663db113335689debe8
irpsv <i.rpsv@live.com>
2017-02-26 16:20:59 +0500

    update GeneratorTest

--- tests/generator.php
+++ tests/generator.php
@@ -45,16 +45,16 @@ class GeneratorTest extends TestCase
     {
         $this->createTables();
 
-        echo Generator::buildClassMysql('post_category');
-        echo Generator::buildClassMysql('post');
+        $codePost = preg_replace("/\r\n|\n/", " ", Generator::buildClassMysql('post'));
+        $codeCategory = preg_replace("/\r\n|\n/", " ", Generator::buildClassMysql('post_category'));
 
-        // $this->assertEquals(
-        //     Generator::buildClassMysql('post_category'),
-        //     ""
-        // );
-        // $this->assertEquals(
-        //     Generator::buildClassMysql('post'),
-        //     ""
-        // );
+        $this->assertEquals(
+            $codePost,
+            "<?php  use jugger\\ar\\ActiveRecord; use jugger\\ar\\relations\\OneRelation; use jugger\\model\\field\\DatetimeField; use jugger\\model\\field\\FloatField; use jugger\\model\\field\\IntField; use jugger\\model\\field\\TextField; use jugger\\model\\validator\\PrimaryValidator; use jugger\\model\\validator\\RangeValidator; use jugger\\model\\validator\\RequireValidator;  class Post extends ActiveRecord {     public static function getTableName(): string;     {         return 'post';     }      public static function getSchema(): array     {         return [             new IntField([                 'name' => 'id',                 'validators' => [                     new PrimaryValidator(),                 ],             ]),             new IntField([                 'name' => 'category_id',                 'validators' => [                     new RequireValidator(),                 ],             ]),             new TextField([                 'name' => 'title',                 'validators' => [                     new RequireValidator(),                     new RangeValidator(0, 100),                 ],             ]),             new TextField([                 'name' => 'content',                 'validators' => [                     new RangeValidator(0, 65535),                 ],             ]),             new DatetimeField([                 'name' => 'time',                 'value' => 'CURRENT_TIMESTAMP',                 'format' => 'timestamp',                 'validators' => [                     new RequireValidator(),                 ],             ]),             new FloatField([                 'name' => 'raiting',                 'validators' => [                     new RequireValidator(),                 ],             ]),             new IntField([                 'name' => 'views',             ]),         ];     }          public static function getRelations(): array     {         return [             'post_category' => new OneRelation('category_id', 'id', 'PostCategory'),         ];     }      } "
+        );
+        $this->assertEquals(
+            $codeCategory,
+            "<?php  use jugger\\ar\\ActiveRecord; use jugger\\ar\\relations\\ManyRelation; use jugger\\model\\field\\DatetimeField; use jugger\\model\\field\\IntField; use jugger\\model\\field\\TextField; use jugger\\model\\validator\\PrimaryValidator; use jugger\\model\\validator\\RangeValidator; use jugger\\model\\validator\\RequireValidator;  class PostCategory extends ActiveRecord {     public static function getTableName(): string;     {         return 'post_category';     }      public static function getSchema(): array     {         return [             new IntField([                 'name' => 'id',                 'validators' => [                     new PrimaryValidator(),                 ],             ]),             new TextField([                 'name' => 'name',                 'validators' => [                     new RequireValidator(),                     new RangeValidator(0, 50),                 ],             ]),             new DatetimeField([                 'name' => 'time',                 'value' => 'CURRENT_TIMESTAMP',                 'format' => 'Y-m-d H:i:s',                 'validators' => [                     new RequireValidator(),                 ],             ]),         ];     }          public static function getRelations(): array     {         return [             'posts' => new ManyRelation('id', 'category_id', 'Post'),         ];     }      } "
+        );
     }
 }
