5aa25eb0a66a864ed502511e124e916e80879d7e
irpsv <i.rpsv@live.com>
2017-02-26 16:08:42 +0500

    add MigrationTest

--- tests/migration.php
+++ tests/migration.php
@@ -9,17 +9,17 @@ class MigrationTest extends TestCase
 {
     public function testBase()
     {
-        // $this->assertEquals(
-        //     Migration::getCreateTableSql(Post::class),
-        //     "CREATE TABLE `post`(id INT NOT NULL PRIMARY KEY,title VARCHAR(100) NOT NULL,content TEXT )"
-        // );
-        // $this->assertEquals(
-        //     Migration::getCreateTableSql(Post::class),
-        //     "CREATE TABLE `post`(id INT NOT NULL PRIMARY KEY,title VARCHAR(100) NOT NULL,content TEXT )"
-        // );
-        // $this->assertEquals(
-        //     Migration::getCreateTableSql(Post::class),
-        //     "CREATE TABLE `post`(id INT NOT NULL PRIMARY KEY,title VARCHAR(100) NOT NULL,content TEXT )"
-        // );
+        $this->assertEquals(
+            Migration::getCreateTableSql(Post::class),
+            "CREATE TABLE `post`(`id` INT NOT NULL PRIMARY KEY,`title` VARCHAR(100) NOT NULL,`content` TEXT DEFAULT 'empty content')"
+        );
+        $this->assertEquals(
+            Migration::getCreateTableSql(Section::class),
+            "CREATE TABLE `section`(`id` INT NOT NULL PRIMARY KEY,`name` TEXT )"
+        );
+        $this->assertEquals(
+            Migration::getCreateTableSql(SectionElement::class),
+            "CREATE TABLE `section_element`(`id` INT NOT NULL PRIMARY KEY,`id_element` INT NOT NULL,`id_section` INT NOT NULL,CONSTRAINT `fk_section_element_id_section_to_section_id` FOREIGN KEY (`id_section`) REFERENCES `section`(`id`),CONSTRAINT `fk_section_element_id_element_to_element_id` FOREIGN KEY (`id_element`) REFERENCES `element`(`id`))"
+        );
     }
 }
